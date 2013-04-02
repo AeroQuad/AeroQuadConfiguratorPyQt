03ae1aaa0443edd3eac65e78979f015c00f12140
Ted Carancho <ted@carancho.com>
2013-04-02 00:22:38 -0700

    experimenting with calibration wizards

---
+++ subpanel/dynamicConfig/dynamicConfig.py
@@ -0,0 +1,34 @@
+'''
+Created on Mar 27, 2013
+
+@author: Ted Carancho
+'''
+
+from PyQt4 import QtCore, QtGui
+from subpanel.subPanel import SubPanel
+from subpanel.dynamicConfig.dynamicConfigWindow import Ui_DynamicConfig
+
+class DynamicConfig(QtGui.QWidget, SubPanel):
+    '''Tutorial example for creating your first subpanel. 
+    This example will retrieve the AeroQuad flight software 
+    version number and write it into a label when a push button is pressed.
+    '''
+    
+    def __init__(self):
+        '''
+        This initializes the tutorial subpanel
+        '''
+        QtGui.QWidget.__init__(self)
+        SubPanel.__init__(self)
+        self.ui = Ui_DynamicConfig()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.sendMiniConfig)
+        
+        palette = QtGui.QPalette()
+        palette.setColor(QtGui.QPalette.Foreground, QtCore.Qt.white)
+        self.ui.receiverTitle.setPalette(palette)
+        self.ui.motorTitle.setPalette(palette)
+        
+         
+    def sendMiniConfig(self):
+        pass
