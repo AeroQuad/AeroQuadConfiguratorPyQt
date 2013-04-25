94793ead1be36ddfcd570d6b92c44f545a923b57
Kenny9999 <patricemainville@hotmail.com>
2013-04-24 20:49:01 -0400

    Some model, connection state, observer work

--- utilities/observers/Observer.py
+++ utilities/observers/Observer.py
@@ -5,9 +5,13 @@ Created on Apr 22, 2013
 '''
 
 class Obeserver(object):
-    '''
-    classdocs
-    '''
+            
+    def __init__(self,name=None):
+        self.name = name
+     
+    def method(self,sender,event,msg=None):
+        print "[{0}] got event {1} with message {2}".format(self.name,event,msg)
+    
 
 
         
